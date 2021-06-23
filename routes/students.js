1fd5b57aefeab5a799da82d708efcb020dbabb4b
Peerless Josh 不二 <delacruzjoshua691@gmail.com>
2021-06-23 14:57:14 +0800

    fixing some errors

--- routes/students.js
+++ routes/students.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
     cb(null, "./uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, Date.now() + file.originalname);
   },
 });
 const upload = multer({ storage: storage });
